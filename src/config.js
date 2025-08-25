7e08d75692e0ecde6e2722d3304b0733cb9b44cc
Austin Roos <acroos@hey.com>
2025-08-24 21:34:34 -0700

    fix comments in config file

--- src/config.js
+++ src/config.js
@@ -6,13 +6,17 @@ const CONFIG = {
   publicKey: process.env.PUBLIC_KEY || 'your-public-key', // Your Discord public key
   discordToken: process.env.DISCORD_TOKEN || 'your-discord-token', // Your Discord bot token
   loungeChannelId: {
+    // Main
     '465846009164070912': '1408117493251182713',
+    // FrogBot test
     '1401765937241395273': '1401765962679844926',
-  }, // Discord Channel ID for the Risk Competitive Lounge
+  },
   loungeRoleId: {
+    // Main
     '465846009164070912': '1408116717627904020',
+    // FrogBot test
     '1401765937241395273': '1401766016836571137',
-  }, // Discord Role ID for the Risk Competitive Lounge
+  },
   friendsOfRiskApiBaseUrl: process.env.FRIENDS_OF_RISK_API_BASE_URL || 'https://friendsofrisk.com/api', // Base URL for Friends of Risk API
   friendsOfRiskApiKey: process.env.FRIENDS_OF_RISK_API_KEY || 'your-friends-of-risk-api-key', // API key for Friends of Risk
   redisHost: process.env.REDIS_HOST || 'redis',
